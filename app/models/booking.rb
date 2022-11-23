e102eecc6abff1cde9686d544d05481bb40d131e
georges ryssen <georges.ryssen@gmail.com>
2022-11-23 15:39:14 +0100

    added a gain method

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -5,4 +5,9 @@ class Booking < ApplicationRecord
   validates :pull_id, presence: true
   validates :start_date, presence: true
   validates :end_date, presence: true
+
+  def self.amount(booking)
+    calcul = ((booking.end_date - booking.start_date) * booking.pull.price).to_i
+    return calcul
+  end
 end
