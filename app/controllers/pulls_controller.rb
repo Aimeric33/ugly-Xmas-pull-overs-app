32d9a3e65a7e4f36f4d50aef865b7a93fda7a32e
georges ryssen <georges.ryssen@gmail.com>
2022-11-23 13:10:42 +0100

    add cloudinary in production for heroku

--- app/controllers/pulls_controller.rb
+++ app/controllers/pulls_controller.rb
@@ -12,6 +12,7 @@ class PullsController < ApplicationController
 
   def create
     @pull = Pull.new(pull_params)
+    @pull.user = current_user
     @pull.available = true
     if @pull.save
       redirect_to pull_path(@pull)
