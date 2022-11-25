fe249dba5ace7fecfbfcb0061f26b0244eacca01
Aimeric Marchau <marchauaimeric@gmail.com>
2022-11-25 13:02:29 +0100

    display stats

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -6,8 +6,16 @@ class Booking < ApplicationRecord
   validates :start_date, presence: true
   validates :end_date, presence: true
 
-  def self.amount(booking)
-    calcul = ((booking.end_date - booking.start_date) * booking.pull.price).to_i
+  def self.amount_of_one(booking)
+    calcul = ((booking.end_date - booking.start_date + 1) * booking.pull.price).to_i
     return calcul
+  end
+
+  def self.amount_all(bookings)
+    total = 0
+    bookings.each do |booking|
+      total += Booking.amount_of_one(booking)
+    end
+    total
   end
 end
